7ebee0de6d5e7efe116cb378cda9e9889448bc38
D_W13 <wang.y.derek@gmail.com>
2020-06-04 00:15:11 -0400

    Updates

---
+++ precache-manifest.215c9da844c76e07fcbdfca9784dded6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d5ed69e25f490277f011f1b25626783",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "9aa4fa71c4aa3bc185a0",
+    "url": "/ReactPortfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "c822cc6909729660cb73",
+    "url": "/ReactPortfolio/static/css/main.a9347b7a.chunk.css"
+  },
+  {
+    "revision": "9aa4fa71c4aa3bc185a0",
+    "url": "/ReactPortfolio/static/js/2.e17faadb.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/ReactPortfolio/static/js/2.e17faadb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c822cc6909729660cb73",
+    "url": "/ReactPortfolio/static/js/main.44eb7bd9.chunk.js"
+  },
+  {
+    "revision": "4827475623b104f88743",
+    "url": "/ReactPortfolio/static/js/runtime-main.b209c55f.js"
+  },
+  {
+    "revision": "6ccf59dbd937f45ced7abe72540ad3ec",
+    "url": "/ReactPortfolio/static/media/blockContract.6ccf59db.PNG"
+  },
+  {
+    "revision": "140ecb81a47476e9ff8c9ab896145296",
+    "url": "/ReactPortfolio/static/media/dermabox.140ecb81.PNG"
+  },
+  {
+    "revision": "5ffdcced5b64a3517d0ecfb3030ce5a9",
+    "url": "/ReactPortfolio/static/media/myAvatar.5ffdcced.png"
+  }
+]);
