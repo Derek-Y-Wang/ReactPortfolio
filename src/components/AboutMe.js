9daee2c1111b3e34eea8990e51f2f5322ab58bb1
Derek Wang <wang.y.derek@gmail.com>
2020-06-14 23:54:24 -0400

    started about page

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,11 +1,25 @@
 import React, {Component} from 'react';
+import { Grid, Cell } from 'react-mdl';
+import HeadShot from './img/placeHolder.png';
 
 
 class About extends Component {
     render(){
         return(
-            <div>
-                <h1>About Page</h1>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Grid className="about-grid">
+                    <Cell col={12}>
+                        <img shadow={5} src={HeadShot}
+                        alt="avatar"
+                        className="avatar-img"/>
+                        <div className="about-text">
+                            <h1>Hi, I'm Derek Wang, a student at the University of Toronto.
+                                There I study Computer Science and Statistical Sciences.
+                                I am interested in ... Most important of all I love to learn.
+                            </h1>                        
+                        </div>
+                    </Cell>
+                </Grid>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import 'react-mdl/extra/material.js'
 import 'react-mdl/extra/material.css'
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 
 
